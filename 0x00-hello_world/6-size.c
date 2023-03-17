609217e92955ac9d541e6cfd01aa7e38e7b4037f
Tifey23 <emmybeam8@gmail.com>
2023-03-16 18:05:36 -0700

    Finsh task 5

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Description: Using the sizeof functiion
+ *
+ * Result: Always 0(success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d byte\(s\)", sizeof(char));
+}
