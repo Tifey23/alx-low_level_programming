d6197c79e380ab6c94c414feb40b135c39012ecf
Tifey23 <emmybeam8@gmail.com>
2023-03-18 16:57:08 -0700

    Correction 2 for printing last digit c Program

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,10 +26,10 @@ int main(void)
 
 	printf(" last digit is %d\n", last_digit);
 	if (last_digit > 5)
-		printf("%d and is grater than 5\n", last_digit);
+		printf("%d and is greater than 5\n", last_digit);
 	else if (last_digit == 0)
 		printf("%d and is 0", last_digit);
-	else if (last_digit < 6 & last_digit != 0)
+	else if ((last_digit < 6) & (last_digit != 0))
 		printf("%d and is less than 6 and not 0\n", last_digit);
 	return (0);
 }
