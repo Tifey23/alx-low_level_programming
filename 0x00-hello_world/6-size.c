f6ff95f60497ef9d06dcc564cb81368318809018
Tifey23 <emmybeam8@gmail.com>
2023-03-20 15:36:50 -0700

    Hello World c Program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,5 +9,16 @@
 
 int main(void)
 {
-	printf("Size of a char: %d byte\(s\)", sizeof(char));
+	char l;
+	int number;
+	long int longinteger;
+	long long int longerInteger;
+	float Decimal;
+
+	printf("Size of a char: %d byte(s)", sizeof(l));
+	printf("Size of an int: %d byte(s)", sizeof(number));
+	printf("Size of a long int: %d byte(s)", sizeof(longinteger));
+	printf("Size of a long long int: %d byte(s)", sizeof(longerInteger));
+	printf("Size of a float: %c byte(s)", sizeof(Decimal));
+	return (0);
 }
