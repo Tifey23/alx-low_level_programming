b27f5cdf1008121a415611dbb79d078f46fde653
Tifey23 <emmybeam8@gmail.com>
2023-03-20 15:42:36 -0700

    Correction 2 for 6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry
+ * main - Entry point
  *
  * Description: 'Using the sizeof functiion'
  *
- * Result: Always 0(success)
+ * Return: Always 0(success)
  */
 
 int main(void)
