27a94c68d717623e3e0363d7823c3a7d840318d4
Tifey23 <emmybeam8@gmail.com>
2023-03-20 18:14:34 -0700

    Advanced projects in C hello world program

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: 'Escpaing charaters'
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - DoraKorpar, 2015-10-19\n";
+
+	puts(quote);
+	return (1);
+}
