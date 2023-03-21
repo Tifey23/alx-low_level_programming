e700a883417f1bd213433081edcae74eb188aa46
Tifey23 <emmybeam8@gmail.com>
2023-03-21 06:49:36 -0700

    correction 1 for tasl 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -18,6 +17,7 @@ void printLowercase(void);
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
